0a9ab258ab7bc724f8b9ac94b9200f2887f2cf4f
Ayan <ajan.kohan@gmail.com>
2023-05-27 04:22:49 +0600

    m4.2 task3

--- src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
@@ -2,6 +2,19 @@ package school.mjc.stage0.loops.task3;
 
 public class DigitsSum {
     public void printDigitsSum(int t){
+        int value = t;
+        String str = Integer.toString(value);
+        int result =0;
+        for(int i = str.length(); i>1;i--){
+            //89
+            int pow = (int) Math.pow(10,i-1);
+            int r = value/pow;
+            value =t%pow;
+            result += r;
+        }
+        result += value;
+        System.out.println(Math.abs(result));
+
 
     }
 }
--- src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
@@ -2,6 +2,14 @@ package school.mjc.stage0.loops.task3;
 
 public class NinesSum {
     public void calculateSum(int lengthOfLastNumber) {
-
+        int result = 0;
+        for (int i =1; i<=lengthOfLastNumber; i++){
+            String nine = "";
+            for (int j =0; j<i; j++){
+                nine += "9";
+            }
+            result += Integer.parseInt(nine);
+        }
+        System.out.println(result);
     }
 }
